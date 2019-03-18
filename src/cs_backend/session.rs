7ab74286d6ea914a1a98ad125c7b3d4a8972835c
Eisha Chen-yen-su <chenyensu0@gmail.com>
2019-03-18 20:16:42 +0100

    App: Add the ability to connect and disconnect
    This adds the ability to connect and disconnect from an account
    on an arbitrary homeserver. The device name of the newly created
    session is "Matrix visualisations".

--- src/cs_backend/session.rs
+++ src/cs_backend/session.rs
@@ -1,34 +1,35 @@
 #[derive(Clone, Debug)]
 pub struct Session {
     pub server_name: String,
+    pub room_id: String,
 
     pub username: String,
     pub user_id: String,
     pub password: String,
-    pub access_token: String,
+    pub access_token: Option<String>,
 
-    pub device_id: String,
-    pub room_id: String,
-    pub filter_id: String,
-    pub next_batch_token: String,
-    pub prev_batch_token: String,
+    pub device_id: Option<String>,
+    pub filter_id: Option<String>,
+    pub next_batch_token: Option<String>,
+    pub prev_batch_token: Option<String>,
 }
 
 impl Session {
     pub fn empty() -> Self {
         Session {
             server_name: String::new(),
+            room_id: String::new(),
 
             username: String::new(),
             user_id: String::new(),
             password: String::new(),
-            access_token: String::new(),
 
-            device_id: String::new(),
-            room_id: String::new(),
-            filter_id: String::new(),
-            next_batch_token: String::new(),
-            prev_batch_token: String::new(),
+            access_token: None,
+
+            device_id: None,
+            filter_id: None,
+            next_batch_token: None,
+            prev_batch_token: None,
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,74 @@
+extern crate failure;
 extern crate petgraph;
+extern crate serde_derive;
 extern crate yew;
 
 mod cs_backend;
 mod model;
 
-use yew::services::ConsoleService;
-use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
+use failure::Error;
+use yew::services::fetch::FetchTask;
+use yew::services::{ConsoleService, FetchService};
+use yew::{html, Callback, Component, ComponentLink, Html, Renderable, ShouldRender};
 
+use cs_backend::authentication::ConnectionResponse;
 use cs_backend::session::Session;
 
 pub struct Model {
     console: ConsoleService,
+    fetch: FetchService,
+
+    connection_callback: Callback<Result<ConnectionResponse, Error>>,
+    connection_task: Option<FetchTask>,
+
+    disconnection_callback: Callback<Result<(), Error>>,
+    disconnection_task: Option<FetchTask>,
+
     session: Session,
 }
 
 pub enum Msg {
     ServerName(html::ChangeData),
+    RoomId(html::ChangeData),
+
     Username(html::ChangeData),
     Password(html::ChangeData),
-    RoomId(html::ChangeData),
+
     Connect,
     Disconnect,
+
+    Connected(cs_backend::authentication::ConnectionResponse),
+    ConnectionFailed,
+
+    Disconnected,
+    DisconnectionFailed,
 }
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+    fn create(_: Self::Properties, mut link: ComponentLink<Self>) -> Self {
         Model {
             console: ConsoleService::new(),
+            fetch: FetchService::new(),
+
+            connection_callback: link.send_back(|response: Result<ConnectionResponse, Error>| {
+                match response {
+                    Ok(res) => Msg::Connected(res),
+                    Err(_) => Msg::ConnectionFailed,
+                }
+            }),
+            connection_task: None,
+
+            disconnection_callback: link.send_back(|response: Result<(), Error>| {
+                match response {
+                    Ok(_) => Msg::Disconnected,
+                    Err(_) => Msg::DisconnectionFailed,
+                }
+            }),
+            disconnection_task: None,
+
             session: Session::empty(),
         }
     }
@@ -39,6 +78,11 @@ impl Component for Model {
             Msg::ServerName(sn) => {
                 if let html::ChangeData::Value(sn) = sn {
                     self.session.server_name = sn;
+                }
+            }
+            Msg::RoomId(ri) => {
+                if let html::ChangeData::Value(ri) = ri {
+                    self.session.room_id = ri;
                 }
             }
             Msg::Username(u) => {
@@ -51,19 +95,45 @@ impl Component for Model {
                     self.session.password = p;
                 }
             }
-            Msg::RoomId(ri) => {
-                if let html::ChangeData::Value(ri) = ri {
-                    self.session.room_id = ri;
-                }
-            }
             Msg::Connect => {
-                self.session.user_id =
-                    format!("@{}:{}", self.session.username, self.session.server_name);
+                let task = self.connect(self.connection_callback.clone());
+                self.connection_task = Some(task);
 
                 self.console.log(&format!("Info: {:?}", self.session));
             }
             Msg::Disconnect => {
                 self.console.log("Disconnecting");
+
+                let task = self.disconnect(self.disconnection_callback.clone());
+                self.disconnection_task = Some(task);
+            }
+            Msg::Connected(res) => {
+                self.session.user_id = res.user_id;
+                self.session.access_token = Some(res.access_token);
+                self.session.device_id = Some(res.device_id);
+
+                self.console.log(&format!(
+                    "Connected with token: {} and as {}",
+                    self.session.access_token.as_ref().unwrap(),
+                    self.session.device_id.as_ref().unwrap()
+                ));
+
+                self.connection_task = None;
+            }
+            Msg::ConnectionFailed => {
+                self.console.log("Connection failed");
+
+                self.connection_task = None;
+            }
+            Msg::Disconnected => {
+                self.console.log("Disconnected");
+
+                self.disconnection_task = None;
+            }
+            Msg::DisconnectionFailed => {
+                self.console.log("Could not disconnect");
+
+                self.disconnection_task = None;
             }
         }
 
@@ -77,11 +147,11 @@ impl Renderable<Model> for Model {
             <ul>
                 <li>{ "Server name: " }<input type="text", onchange=|e| Msg::ServerName(e),/></li>
 
+                <li>{ "Room ID: " }<input type="text", onchange=|e| Msg::RoomId(e),/></li>
+
                 <li>{ "Username: " }<input type="text", onchange=|e| Msg::Username(e),/></li>
 
                 <li>{ "Password: " }<input type="password", onchange=|e| Msg::Password(e),/></li>
-
-                <li>{ "Room ID: " }<input type="text", onchange=|e| Msg::RoomId(e),/></li>
 
                 <li><button onclick=|_| Msg::Connect,>{ "Connect" }</button></li>
 
