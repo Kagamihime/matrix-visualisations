127d16a9bad72da7ed90f9a156fc3417d3941c71
Eisha Chen-yen-su <chenyensu0@gmail.com>
2019-03-22 16:59:43 +0100

    model: dag.rs: Build the DAG from a `SyncResponse`

--- src/model/dag.rs
+++ src/model/dag.rs
@@ -4,6 +4,7 @@ use petgraph::graph::{Graph, NodeIndex};
 use petgraph::Directed;
 
 use super::event::Event;
+use crate::cs_backend::events::SyncResponse;
 
 pub struct RoomEvents {
     room_id: String,     // The ID of the room
@@ -11,5 +12,113 @@ pub struct RoomEvents {
 
     dag: Graph<Event, (), Directed>,        // The DAG of the events
     events_map: HashMap<String, NodeIndex>, // Allows to quickly locate an event in the DAG with its ID
-    latest_event: String,                   // The ID of the latest event in the DAG
+    depth_map: HashMap<i64, Vec<NodeIndex>>,
+    latest_event: String, // The ID of the latest event in the DAG
+    earliest_event: String,
+    max_depth: i64,
+    min_depth: i64,
+}
+
+impl RoomEvents {
+    pub fn from_sync_response(
+        room_id: &str,
+        server_name: &str,
+        res: SyncResponse,
+    ) -> Option<RoomEvents> {
+        match res.rooms.join.get(room_id) {
+            Some(room) => {
+                let timeline = &room.timeline.events;
+
+                let timeline: Vec<Event> = timeline
+                    .iter()
+                    .map(|ev| {
+                        serde_json::from_value(ev.clone()).expect(&format!(
+                            "Failed to parse timeline event: {}",
+                            serde_json::to_string(&ev).expect("Failed to fail..."),
+                        ))
+                    })
+                    .collect();
+
+                let mut dag: Graph<Event, (), Directed> = Graph::new();
+                let mut events_map: HashMap<String, NodeIndex> =
+                    HashMap::with_capacity(timeline.len() /*+ state.len()*/);
+                let mut depth_map: HashMap<i64, Vec<NodeIndex>> =
+                    HashMap::with_capacity(timeline.len() /*+ state.len()*/);
+                let mut latest_event = String::new();
+                let mut earliest_event = String::new();
+                let mut max_depth = -1;
+                let mut min_depth = -1;
+
+                for event in timeline.iter() {
+                    let id = &event.event_id;
+                    let depth = event.depth;
+                    let index = dag.add_node(event.clone());
+
+                    events_map.insert(id.clone(), index);
+
+                    match depth_map.get_mut(&depth) {
+                        None => {
+                            depth_map.insert(depth, vec![index]);
+                        }
+                        Some(v) => {
+                            v.push(index);
+                        }
+                    }
+
+                    if latest_event.is_empty() {
+                        latest_event = id.clone();
+                        earliest_event = id.clone();
+                        max_depth = depth;
+                        min_depth = depth;
+                    } else if let Some(latest_index) = events_map.get(&latest_event) {
+                        if let Some(latest_ev) = dag.node_weight(*latest_index) {
+                            if latest_ev < event {
+                                latest_event = event.event_id.clone();
+                                max_depth = event.depth;
+                            }
+                        }
+                    } else if let Some(earliest_index) = events_map.get(&earliest_event) {
+                        if let Some(earliest_ev) = dag.node_weight(*earliest_index) {
+                            if earliest_ev > event {
+                                earliest_event = event.event_id.clone();
+                                min_depth = event.depth;
+                            }
+                        }
+                    }
+                }
+
+                let mut edges: Vec<(NodeIndex, NodeIndex)> = Vec::new();
+
+                // TODO: Refactor with iterators
+                for d in (min_depth..=max_depth).rev() {
+                    if let Some(indices) = depth_map.get(&d) {
+                        for index in indices {
+                            if let Some(src_ev) = dag.node_weight(*index) {
+                                for dst_id in src_ev.get_prev_events() {
+                                    if let Some(dst_index) = events_map.get(dst_id) {
+                                        edges.push((*index, *dst_index));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                dag.extend_with_edges(edges);
+
+                Some(RoomEvents {
+                    room_id: String::from(room_id),
+                    server_name: String::from(server_name),
+                    dag,
+                    events_map,
+                    depth_map,
+                    latest_event,
+                    earliest_event,
+                    max_depth,
+                    min_depth,
+                })
+            }
+            None => None,
+        }
+    }
 }
