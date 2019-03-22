6ee4bc19d9d593863d5cd31fd5ee3f5c6f16b0dd
Eisha Chen-yen-su <chenyensu0@gmail.com>
2019-03-22 16:48:24 +0100

    model: event.rs: Complete the `Event` struct
    Complete the `Event` struct so that it can be properly (de)serialized
    with serde.

--- src/model/event.rs
+++ src/model/event.rs
@@ -1,10 +1,15 @@
+use serde_derive::{Deserialize, Serialize};
+use serde_json::Value as JsonValue;
+
+#[derive(Debug, Default, Clone, Deserialize, Serialize)]
 pub struct Event {
-    room_id: String,          // Room identifier
-    sender: String,           // The ID of the user sending the event
-    origin: String,           // The `server_name` of the homeserver that created this event
+    room_id: String,       // Room identifier
+    sender: String,        // The ID of the user sending the event
+    origin: String,        // The `server_name` of the homeserver that created this event
     origin_server_ts: i64, // Timestamp in milliseconds on origin homeserver when this event was created
-    etype: String,         // Event type
-    prev_events: Vec<String>, // Event IDs for the most recent events in the room that the homeserver was aware of when it made this event
-    depth: i64,               // The maximum depth of the `prev_events`, plus one
-    event_id: String,         // The event ID
+    #[serde(rename = "type")]
+    etype: String, // Event type
+    prev_events: Vec<JsonValue>, // Event IDs for the most recent events in the room that the homeserver was aware of when it made this event
+    pub depth: i64,              // The maximum depth of the `prev_events`, plus one
+    pub event_id: String,        // The event ID
 }
