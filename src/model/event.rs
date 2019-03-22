f9c612d878385989da4419aea50a3e01f623a4b0
Eisha Chen-yen-su <chenyensu0@gmail.com>
2019-03-22 16:50:25 +0100

    model: event.rs: Implement `Ord` and `Eq` traits
    These traits are useful to elegantly compute the "latest" and
    "earliest" events of a DAG.

--- src/model/event.rs
+++ src/model/event.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use serde_derive::{Deserialize, Serialize};
 use serde_json::Value as JsonValue;
 
@@ -13,3 +15,31 @@ pub struct Event {
     pub depth: i64,              // The maximum depth of the `prev_events`, plus one
     pub event_id: String,        // The event ID
 }
+
+impl PartialOrd for Event {
+    fn partial_cmp(&self, other: &Event) -> Option<Ordering> {
+        if self.depth == other.depth {
+            Some(self.origin_server_ts.cmp(&other.origin_server_ts))
+        } else {
+            Some(self.depth.cmp(&other.depth))
+        }
+    }
+}
+
+impl Ord for Event {
+    fn cmp(&self, other: &Event) -> Ordering {
+        if self.depth == other.depth {
+            self.origin_server_ts.cmp(&other.origin_server_ts)
+        } else {
+            self.depth.cmp(&other.depth)
+        }
+    }
+}
+
+impl PartialEq for Event {
+    fn eq(&self, other: &Event) -> bool {
+        self.event_id == other.event_id
+    }
+}
+
+impl Eq for Event {}
