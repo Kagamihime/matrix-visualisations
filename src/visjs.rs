da2c28ad146fea7db9730e45e28db19abc56be53
Eisha Chen-yen-su <chenyensu0@gmail.com>
2019-06-06 19:43:00 +0200

    visjs.rs: Properly update the DAG

--- src/visjs.rs
+++ src/visjs.rs
@@ -1,3 +1,4 @@
+use crate::model::dag::DataSet;
 use stdweb::web;
 use stdweb::web::IParentNode;
 use stdweb::Value;
@@ -5,7 +6,12 @@ use stdweb::Value;
 use crate::model::dag::RoomEvents;
 
 #[derive(Default)]
-pub struct VisJsService(Option<Value>);
+pub struct VisJsService {
+    lib: Option<Value>,
+    data: Option<Value>,
+    earliest_event: String,
+    latest_event: String,
+}
 
 impl VisJsService {
     pub fn new() -> Self {
@@ -13,35 +19,102 @@ impl VisJsService {
             return vis;
         };
 
-        VisJsService(Some(lib))
+        VisJsService {
+            lib: Some(lib),
+            data: None,
+            earliest_event: String::new(),
+            latest_event: String::new(),
+        }
     }
 
-    pub fn display_dag(&self, events_dag: &RoomEvents, id: &str) {
-        let lib = self.0.as_ref().expect("vis library object lost");
+    pub fn display_dag(&mut self, events_dag: &mut RoomEvents, id: &str) {
+        let lib = self.lib.as_ref().expect("vis library object lost");
 
-        let dot_string = events_dag.to_dot();
+        let data = events_dag.create_data_set();
 
         let container = web::document()
             .query_selector(id)
             .expect("Couldnt get document element")
             .expect("Couldn't get document element");
 
-        js! { @(no_return)
+        js_serializable!(DataSet);
+
+        self.data = Some(js! {
             var vis = @{lib};
 
-            var parsedData = vis.network.convertDot(@{dot_string});
+            var d = @{data};
+
+            var nodes = new vis.DataSet(d.nodes);
+            var edges = new vis.DataSet(d.edges);
 
             var data = {
-                nodes: parsedData.nodes,
-                edges: parsedData.edges
+                nodes: nodes,
+                edges: edges
             };
 
-            var options = parsedData.options;
-            options.edges = {
-                length: 500
+            var options = {
+                layout: {
+                    randomSeed: undefined,
+                    improvedLayout: true,
+                    hierarchical: {
+                        enabled: true,
+                        levelSeparation: 200,
+                        nodeSpacing: 300,
+                        direction: "DU",
+                        sortMethod: "directed"
+                    }
+                },
+                nodes: {
+                    shape: "box",
+                    widthConstraint: {
+                        minimum: 150,
+                        maximum: 200
+                    }
+                }
             };
 
             var network = new vis.Network(@{container}, data, options);
-        };
+
+            return data;
+        });
+
+        self.earliest_event = events_dag.earliest_event.clone();
+        self.latest_event = events_dag.latest_event.clone();
+    }
+
+    pub fn update_dag(&mut self, events_dag: &RoomEvents) {
+        if self.earliest_event != events_dag.earliest_event {
+            let data = self.data.as_ref().expect("No data set found");
+            let earlier_events = events_dag.get_earlier_events(self.earliest_event.clone());
+
+            self.data = Some(js! {
+                var data = @{data};
+                var ev = @{earlier_events};
+
+                data.nodes.add(ev.nodes);
+                data.edges.add(ev.edges);
+
+                return data;
+            });
+
+            self.earliest_event = events_dag.earliest_event.clone();
+        }
+
+        if self.latest_event != events_dag.latest_event {
+            let data = self.data.as_ref().expect("No data set found");
+            let new_events = events_dag.get_new_events(self.latest_event.clone());
+
+            self.data = Some(js! {
+                var data = @{data};
+                var ev = @{new_events};
+
+                data.nodes.add(ev.nodes);
+                data.edges.add(ev.edges);
+
+                return data;
+            });
+
+            self.latest_event = events_dag.latest_event.clone();
+        }
     }
 }
