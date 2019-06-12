446d763866818d9f1900c4bb2a8e0cbfa31106f9
Eisha Chen-yen-su <chenyensu0@gmail.com>
2019-06-12 14:32:01 +0200

    Add fields stored in the events
    This adds fields stored in the events so that their JSON bodies are
    retrieved by the application the first time these events are received.

--- src/model/event.rs
+++ src/model/event.rs
@@ -14,9 +14,16 @@ pub struct Event {
     origin_server_ts: i64, // Timestamp in milliseconds on origin homeserver when this event was created
     #[serde(rename = "type")]
     etype: String, // Event type
+    state_key: Option<String>,
+    content: JsonValue,
     prev_events: Vec<JsonValue>, // Event IDs for the most recent events in the room that the homeserver was aware of when it made this event
     pub depth: i64,              // The maximum depth of the `prev_events`, plus one
-    pub event_id: String,        // The event ID
+    auth_events: Vec<JsonValue>,
+    redacts: Option<String>,
+    unsigned: Option<JsonValue>,
+    pub event_id: String, // The event ID
+    hashes: JsonValue,
+    signatures: JsonValue,
 }
 
 impl Event {
